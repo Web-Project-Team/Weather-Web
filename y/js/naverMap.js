f059fa635eb08ed02c744b3aab8488291b8dbb72
HyeYeonCha <gpdus335@naver.com>
2020-07-06 11:11:12 +0900

    bug fix

--- y/js/naverMap.js
+++ y/js/naverMap.js
@@ -124,9 +124,9 @@ function searchAddressToCoordinate(address) {
         htmlAddresses.push("[지번 주소] " + item.jibunAddress);
       }
 
-      // if (item.englishAddress) {
-      //   htmlAddresses.push("[영문명 주소] " + item.englishAddress);
-      // }
+      if (item.englishAddress) {
+        htmlAddresses.push("[영문명 주소] " + item.englishAddress);
+      }
 
       infoWindow.setContent(
         [
@@ -167,52 +167,8 @@ function initGeocoder() {
     searchAddressToCoordinate($("#address").val());
   });
 
-  // searchAddressToCoordinate("정자동 178-1");
-  searchAddressToCoordinate($("#address").val());
+  searchAddressToCoordinate("정자동 178-1");
 }
 
 naver.maps.onJSContentLoaded = initGeocoder;
 naver.maps.Event.once(map, "init_stylemap", initGeocoder);
-
-// 현위치 가져오기
-
-// var infowindow = new naver.maps.InfoWindow();
-
-// function onSuccessGeolocation(position) {
-//   var location = new naver.maps.LatLng(
-//     position.coords.latitude,
-//     position.coords.longitude
-//   );
-
-//   map.setCenter(location); // 얻은 좌표를 지도의 중심으로 설정합니다.
-//   map.setZoom(10); // 지도의 줌 레벨을 변경합니다.
-
-//   infowindow.setContent(
-//     '<div style="padding:20px;">' +
-//       "geolocation.getCurrentPosition() 위치" +
-//       "</div>"
-//   );
-
-//   infowindow.open(map, location);
-//   console.log("Coordinates: " + location.toString());
-// }
-
-// $(window).on("load", function () {
-//   if (navigator.geolocation) {
-//     /**
-//      * navigator.geolocation 은 Chrome 50 버젼 이후로 HTTP 환경에서 사용이 Deprecate 되어 HTTPS 환경에서만 사용 가능 합니다.
-//      * http://localhost 에서는 사용이 가능하며, 테스트 목적으로, Chrome 의 바로가기를 만들어서 아래와 같이 설정하면 접속은 가능합니다.
-//      * chrome.exe --unsafely-treat-insecure-origin-as-secure="http://example.com"
-//      */
-//     navigator.geolocation.getCurrentPosition(
-//       onSuccessGeolocation,
-//       onErrorGeolocation
-//     );
-//   } else {
-//     var center = map.getCenter();
-//     infowindow.setContent(
-//       '<div style="padding:20px;"><h5 style="margin-bottom:5px;color:#f00;">Geolocation not supported</h5></div>'
-//     );
-//     infowindow.open(map, center);
-//   }
-// });
